d426168e19e59b7cf7f78ca257ab1768d80c84cf
Henrik Poulsen <henrik_v_poulsen@hotmail.com>
2019-10-07 10:31:32 +0200

    first

--- app/src/main/java/com/vincentz/retrofitapi/Services/ServiceBuilder.java
+++ app/src/main/java/com/vincentz/retrofitapi/Services/ServiceBuilder.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceBuilder {
-    private static final String URL = "http://192.168.0.11:8080/DynamicWebAPIProjectv3/api/";
+    private static final String URL = "http://192.168.43.232:8080/DynamicWebAPIProjectv3/api/";
     private static Retrofit.Builder builder = new Retrofit.Builder().baseUrl(URL)
                     .addConverterFactory(GsonConverterFactory.create());
     private static Retrofit retrofit = builder.build();
